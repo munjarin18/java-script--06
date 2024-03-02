96f3783772e72c9322c78af4338052ffde932158
Munjarin <anjumanarju2718@gmail.com>
2024-03-02 11:20:16 -0800

    js-06

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let body = document.querySelector("body")
+let main = document.querySelector(".main")
+
+
+body.addEventListener("click",(e)=>{
+
+
+
+
+    if(e.target.classList[0] == "btn"){
+        main.classList.add("main2")
+        main.innerHTML =  `<div><img src= ${e.target.dataset.url} alt=""/>
+        <i class="fa-solid fa-xmark"></i></div> `
+    }else{
+        main.classList.remove("main2")
+        main.innerHTML = ""
+    }
+
+
+
+})
+
